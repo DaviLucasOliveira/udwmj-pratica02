88a6903a4a000eae95e67e912ab1d273d50fd72b
Davi Lucas dos Santos Oliveira <56208920+DaviLucasOliveira@users.noreply.github.com>
2026-08-25 17:12:33 -0300

    Delete Pratica02 directory

--- Pratica02/script.js
+++
@@ -1,7 +0,0 @@
-const nome = prompt("Por favor, digite o seu nome:");
-const elementoSaudacao = document.getElementById("saudacao");
-if (nome && nome.trim() !== " ") {
-    elementoSaudacao.textContent = `Olá, ${nome.trim()}!`;
-} else{
-    elementoSaudacao.textContent = "Olá, visitante!";
-}
